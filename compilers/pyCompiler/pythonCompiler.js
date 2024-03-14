d58e98a1fe2a45e18cde802f5689439d50975cd6
[Vidit Pandey] <viditpandey06@gmail.com>
2024-03-15 00:16:45 +0530

    Update pythonCompiler.js

--- compilers/pyCompiler/pythonCompiler.js
+++ compilers/pyCompiler/pythonCompiler.js
@@ -1,14 +1,15 @@
 const { exec } = require("child_process");
 const fs = require("fs");
 const path = require("path");
-
-const outputPath = path.join(__dirname, "outputs");
+const outputPath='';
+let output=()=>{
+outputPath = path.join(__dirname, "outputs");
 console.log(outputPath);
 if (!fs.existsSync(outputPath)) {
   fs.mkdirSync(outputPath, { recursive: true });
-}
-
+}};
 const executePython = (filepath) => {
+  output();
   const jobId = path.basename(filepath).split(".")[0];
   const outPath = path.join(outputPath, `${jobId}.out`);
 

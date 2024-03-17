4696677fe76ed47a14c022773abaf8836be7902d
[Vidit Pandey] <viditpandey06@gmail.com>
2024-03-17 14:16:54 +0530

    Bug

--- index.js
+++ index.js
@@ -1,58 +1,56 @@
 const path = require("path");
-const executeCpp = require("./compilers/cppCompiler/cppCompiler");
-const {executeJava} = require("./compilers/javaCompiler/javacompiler");
-const executeJavascript = require("./compilers/jsCompiler/jscompiler");
-const { executePython } = require("./compilers/pyCompiler/pythonCompiler");
 const fs = require("fs");
+const {executeCpp} = require("./compilers/cppCompiler/cppCompiler");
+const {executeJava} = require("./compilers/javaCompiler/javacompiler");
+const {executeJavascript} = require("./compilers/jsCompiler/jscompiler");
+const { executePython } = require("./compilers/pyCompiler/pythonCompiler");
 const { v4: uuid } = require("uuid");
 const output="output";
-const dirCodes = path.join(__dirname, "codes");
-console.log("Vidit");
-console.log(dirCodes);
+const filepath=null;
+const dirCodes = path.join(__dirname, "codeBase");
+console.log("DirCodes:",dirCodes);
 if (!fs.existsSync(dirCodes)) {
   fs.mkdirSync(dirCodes, { recursive: true });
 }
-
-const generateFile = async () => {
+const generateFile =(language, code) => {
   const jobId = uuid();
-  format='py'
-  content='print("Hello Vidit")'
-  const filename = `${jobId}.${format}`;
-  const filepath = path.join(dirCodes, filename);
-  await fs.writeFileSync(filepath, content);
-  return filepath;
-};
-const filepath = generateFile();
+  if (language === "cpp") {
+    const filename = `${jobId}.cpp`;
+    const filepath =   path.join(dirCodes, filename);
+    fs.writeFileSync(filepath, code);
+    
+  } else if (language === "java") {
+    const filename = `${jobId}.java`;
+    const filepath = path.join(dirCodes, filename);
+    fs.writeFileSync(filepath, code);
+  }
+  else if (language === "js") {
+    const filename = `${jobId}.js`;
+    const filepath = path.join(dirCodes, filename);
+    fs.writeFileSync(filepath, code);
+  }
+  else if (language === "py") {
+    const filename = `${jobId}.py`;
+    const filepath = path.join(dirCodes, filename);
+    fs.writeFileSync(filepath, code);
+}
+}
+generateFile("cpp", "#include <iostream>\n using namespace std;\nint main() {\n  cout << \"Hello, World!\";\n  return 0;\n}");
 
 let cppcompiler = async () => {
   const output = await executeCpp(filepath);
   console.log(output);
 };
 
-let javacompiler = async (filepath) => {
-  const output = await executeJava(filepath);
-  console.log(output);
-};
-let jscompiler = async () => {
-  const output = await executeJavascript(filepath);
-  console.log(output);
-};
-let pythoncompiler = async () => {
-  const output = await executePython(filepath);
-  console.log(output);
-};
-// pythoncompiler();
-
-const runPython = async () => {
-  try {
-    const filepath = await generateFile();
-    console.log("Python Filepath:", filepath);
-    const output = await executePython(filepath);
-    console.log("Python Output:", output);
-  } catch (error) {
-    console.error("Error:", error);
-  }
-};
-
-//runPython();
-javacompiler('C:\\Users\\vidit\\Desktop\\VE_Compiler\\codes\\p1.java');
+// let javacompiler = async (filepath) => {
+//   const output = await executeJava(filepath);
+//   console.log(output);
+// };
+// let jscompiler = async () => {
+//   const output = await executeJavascript(filepath);
+//   console.log(output);
+// };
+// let pythoncompiler = async () => {
+//   const output = await executePython(filepath);
+//   console.log(output);
+// };
---
+++ tempCodeRunnerFile.js
@@ -0,0 +1,12 @@
+let javacompiler = async (filepath) => {
+//   const output = await executeJava(filepath);
+//   console.log(output);
+// };
+// let jscompiler = async () => {
+//   const output = await executeJavascript(filepath);
+//   console.log(output);
+// };
+// let pythoncompiler = async () => {
+//   const output = await executePython(filepath);
+//   console.log(output);
+// };

ad0c1aba6dfc2483e42d6c8b077ddb592a6fb773
[Vidit Pandey] <viditpandey06@gmail.com>
2024-03-18 19:06:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const path = require("path");
 const fs = require("fs");
 const {executeCpp} = require("./compilers/cppCompiler/cppCompiler");
-const {executeJava} = require("./compilers/javaCompiler/javacompiler");
+const {executeJava} = require("./compilers/javaCompiler/javaCompiler");
 const {executeJavascript} = require("./compilers/jsCompiler/jscompiler");
 const { executePython } = require("./compilers/pyCompiler/pythonCompiler");
 const { v4: uuid } = require("uuid");
@@ -14,33 +14,35 @@ if (!fs.existsSync(dirCodes)) {
 }
 
 
-const generateFile =(language, code) => {
-  const jobId = uuid();
+const generateFile = async (language, code) => {
+  
   if (language === "cpp") {
+    const jobId = uuid();
     const filename = `${jobId}.cpp`;
     const filepath =   path.join(dirCodes, filename);
     fs.writeFileSync(filepath, code);
     executeCpp(filepath);
     
   } else if (language === "java") {
+    jobId="Myclass1";
     const filename = `${jobId}.java`;
     const filepath = path.join(dirCodes, filename);
     fs.writeFileSync(filepath, code);
+    await executeJava(filepath);
   }
   else if (language === "js") {
+    const jobId = uuid();
     const filename = `${jobId}.js`;
     const filepath = path.join(dirCodes, filename);
     fs.writeFileSync(filepath, code);
   }
   else if (language === "py") {
+    const jobId = uuid();
     const filename = `${jobId}.py`;
     const filepath = path.join(dirCodes, filename);
     fs.writeFileSync(filepath, code);
 }
 }
 generateFile("cpp", "#include <iostream>\n using namespace std;\nint main() {\n  cout << \"Hello, World!\";\n  return 0;\n}");
+generateFile("java", "public class Myclass1 {\n  public static void main(String[] args) {\n    System.out.println(\"Hello, World\");\n  }\n}");
 
-let cppcompiler = async () => {
-  const output = await executepp(filepath);
-  console.log(output);
-};

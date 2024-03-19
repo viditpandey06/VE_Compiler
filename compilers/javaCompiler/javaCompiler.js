404d6a231acbf7ae448480dd6704314de60f9634
[Vidit Pandey] <viditpandey06@gmail.com>
2024-03-19 23:20:04 +0530

    Update javaCompiler.js
    Updated java compiler to return output along with source path or error incase of error.

--- compilers/javaCompiler/javaCompiler.js
+++ compilers/javaCompiler/javaCompiler.js
@@ -1,5 +1,52 @@
+// const { exec } = require("child_process");
+// const { log } = require("console");
+// const fs = require("fs");
+// const path = require("path");
+
+// const executeJava = async (filepath) => {
+//   try {
+//     const jobId = "Main"; //path.basename(filepath).split(".")[0];
+//     const codebasePath = path.join(__dirname, "../../codebase");
+//     console.log("CodebasePath:", codebasePath);
+//     const outFilePath = path.join(codebasePath, `${jobId}`);
+//     console.log("OutFilePath:", outFilePath);
+//     if (!fs.existsSync(codebasePath)) {
+//       fs.mkdirSync(codebasePath, { recursive: true });
+//     }
+    
+//     const compilationCommand = `javac ${filepath} `;
+    
+//     await execPromise(compilationCommand);
+
+//     const executionCommand = `java -cp ${codebasePath} ${jobId}`;
+//     const { stdout, stderr } = await execPromise(executionCommand);
+//     console.log(stdout);
+
+//     return outFilePath;
+//   } catch (error) {
+    
+//     console.log("Error", error);
+//   }
+// };
+
+// const execPromise = (command) => {
+//   return new Promise((resolve, reject) => {
+//     exec(command, (error, stdout, stderr) => {
+//       if (error) {
+//         console.error(error);
+//         reject(error);
+//         return;
+//       }
+//       resolve({ stdout, stderr });
+//     });
+//   });
+// };
+
+
+// module.exports = {
+//   executeJava,
+// };
 const { exec } = require("child_process");
-const { log } = require("console");
 const fs = require("fs");
 const path = require("path");
 
@@ -13,19 +60,19 @@ const executeJava = async (filepath) => {
     if (!fs.existsSync(codebasePath)) {
       fs.mkdirSync(codebasePath, { recursive: true });
     }
-    
+
     const compilationCommand = `javac ${filepath} `;
-    
+
     await execPromise(compilationCommand);
 
     const executionCommand = `java -cp ${codebasePath} ${jobId}`;
     const { stdout, stderr } = await execPromise(executionCommand);
     console.log(stdout);
 
-    return outFilePath;
+    return { outFilePath, stdout }; // Return stdout along with outFilePath
   } catch (error) {
-    
     console.log("Error", error);
+    throw error; // Re-throw error to handle it in the caller
   }
 };
 
@@ -41,7 +88,6 @@ const execPromise = (command) => {
     });
   });
 };
-
 
 module.exports = {
   executeJava,

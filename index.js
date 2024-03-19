2d75f6d1de00a645dfc92c8ac1e774b25f5bd93f
Eshan Singh <63192550+eshan-singh78@users.noreply.github.com>
2024-03-19 23:33:10 +0530

    changed generate file to compile file in index.js

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const { executeJavaScript } = require("./compilers/jsCompiler/jscompiler");
 const { executePython } = require("./compilers/pyCompiler/pythonCompiler");
 const { v4: uuid } = require("uuid");
 const { executeC } = require("./compilers/cCompiler/cCompiler");
+const { compileFunction } = require("vm");
 
 const output = "output";
 const dirCodes = path.join(__dirname, "codeBase");
@@ -15,7 +16,7 @@ if (!fs.existsSync(dirCodes)) {
   fs.mkdirSync(dirCodes, { recursive: true });
 }
 
-const generateFile = async (language, code) => {
+const CompileFile = async (language, code) => {
   let jobId;
   if (language === "cpp") {
     jobId = uuid();
@@ -52,28 +53,28 @@ const generateFile = async (language, code) => {
 
 (async () => {
   try {
-    const cppOutput = await generateFile(
+    const cppOutput = await CompileFile(
       "cpp",
       '#include <iostream>\n using namespace std;\nint main() {\n  cout << "Hello, World!";\n  return 0;\n}'
     );
     console.log("Output of CPP:", cppOutput);
 
-    const javaOutput = await generateFile(
+    const javaOutput = await CompileFile(
       "java",
       'public class Main {\n  public static void main(String[] args) {\n    System.out.println("Hello, World");\n  }\n}'
     );
     console.log("Output of Java:", javaOutput);
 
-    const jsOutput = await generateFile(
+    const jsOutput = await CompileFile(
       "js",
       "console.log('Hello, World! js');"
     );
     console.log("Output of JS:", jsOutput);
 
-    const pyOutput = await generateFile("py", 'print("Hello, World1111ps")');
+    const pyOutput = await CompileFile("py", 'print("Hello, World1111ps")');
     console.log("Output of Python:", pyOutput);
 
-    const cOutput = await generateFile(
+    const cOutput = await CompileFile(
       "c",
       '#include <stdio.h>\n int main() {\n  printf("Hello, World! c ");\n  return 0;\n}'
     );
